62e3b7ce9e859edb0a947849e08b16f61d5fc868
bhaktibj <bhaktibj402@gmail.com>
2020-08-17 16:05:32 +0530

    added new component

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import React, { Component } from 'react';
-// import GreetUser from './components/GreetUser';
-import ProductList from './components/ProductList'
+import GreetUser from './components/GreetUser';
+// import ProductList from './components/ProductList';
+import LoginContainer from './containers/LoginContainer';
 class App extends Component{
   render(){
   return (
     <div className="App">
+      <GreetUser name="Bhakti" surname="Jadhav"/>
     <div className="container d-flex align-items-center flex-column">
-      <ProductList />
+      {/* <ProductList /> */}
+      <LoginContainer />
       </div>
     </div>
   );
